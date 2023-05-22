7daaf2e3a10825238adfebb1c276d4e2ea7509f9
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-22 15:15:00 -0500

    my info in drop-down menu done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,39 +2,46 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
+import man from "../../assets/man.jpg";
 
 export const Header = () => {
     const user = useSelector((state) => state.user.user);
 
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand as={Link} to="/">TEST ASSIGNMENT</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">TEST ASSIGNMENT</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Menu" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={NavLink} to="/">Home</NavDropdown.Item>
+                            <NavDropdown.Item as={NavLink} to="/about">About me</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                <div>
+                                    <Image src={man} roundedCircle width={30} height={30} className="mr-1" />
+                                    <p>Dmitry</p>
+                                    <p>Houston, TX 77084</p>
 
-                        </Nav>
-                        <Nav>
-                            <NavDropdown title="Menu" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={NavLink} to="/">Home</NavDropdown.Item>
-                                <NavDropdown.Item as={NavLink} to="/about">About me</NavDropdown.Item>
-                                {user && (
-                                    <React.Fragment>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item>
-                                            <Image src={user.avatar} roundedCircle width={30} height={30} className="mr-1" />
-                                            {user.name}
-                                        </NavDropdown.Item>
-                                        <NavDropdown.Item>{user.email}</NavDropdown.Item>
-                                    </React.Fragment>
-                                )}
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                                </div>
+                            </NavDropdown.Item>
+                            {user && (
+                                <React.Fragment>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item>
+                                        <Image src={user.avatar} roundedCircle width={30} height={30} className="mr-1" />
+                                        {user.name}
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item>{user.email}</NavDropdown.Item>
+                                </React.Fragment>
+                            )}
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
